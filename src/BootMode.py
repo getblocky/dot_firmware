025d5ec6442470a69d4e856498d885c9c2e5d5a7
curlyz <curly.saigonese@gmail.com>
2018-12-07 21:21:16 +0700

    Add support for Unicode AP

--- src/BootMode.py
+++ src/BootMode.py
@@ -6,9 +6,12 @@ class BootMode :
 	def __init__ (self):
 		self.wlan_ap =  core.network.WLAN(core.network.AP_IF)
 		self.wlan_sta =  core.network.WLAN(core.network.STA_IF)
+		self.wlan_sta.active(True)
+		self.wlan_ap.active(True)
 		self.status = 'start'
 		self.content = ''
-		self.success = False
+		self.success = False #dummy
+		self.server = None
 	async def connect(self, ssid, password):
 		self.wlan_sta.active(True)
 		self.wlan_sta.connect(ssid, password)
@@ -23,8 +26,6 @@ class BootMode :
 		if self.wlan_sta.isconnected():
 			#indicator.animate('pulse',(0,100,50),10)
 			print('\nConnected. Network config:', self.wlan_sta.ifconfig())
-			while not self.success :
-				await core.asyncio.sleep_ms(200)
 			self.wifi_status = 1
 			return True
 		else : 
@@ -50,19 +51,54 @@ class BootMode :
 		
 		
 	def _httpHandlerCheckStatus(self, httpClient, httpResponse):
-		print('Get check status request')
-		if core.flag.wifi == True:
-			content = 'OK'
-			self.success = True
-		elif core.flag.wifi == False:
-			content = 'Failed'
-		else:	
-			content = ''
-		
-		httpResponse.WriteResponseOk(headers = None,
-			contentType	 = "text/html",
-			contentCharset = "UTF-8",
-			content = content)
+		httpResponse.WriteResponseOk(headers = None,contentType= "text/html",	contentCharset = "UTF-8",content = 'OK')
+		print('checking')
+		if self.wlan_sta.isconnected():
+			self.wifi_status = 1
+			print('Connected to ' , request_json['ssid'])
+			config = {}
+			try :
+				config = core.json.loads(open('config.json').read())
+			except :
+				pass
+			if not config.get('known_networks'):
+				config['known_networks'] = [{'ssid': request_json['ssid'], 'password': request_json['password']}]
+			else:
+				exist_ssid = None
+				for n in config['known_networks']:
+					if n['ssid'] == request_json['ssid']:
+						exist_ssid = n
+						break
+				if exist_ssid:
+					exist_ssid['password'] = request_json['password'] # update WIFI password
+					print('Update wifi password')
+				else:
+					# add new WIFI network
+					config['known_networks'].append({'ssid': request_json['ssid'], 'password': request_json['password']})
+					print('Add new network')
+			if len(request_json['token']):
+				config['token'] = request_json['token']
+			
+			f = open('config.json', 'w')
+			f.write(core.json.dumps(config))
+			f.close()
+			core.flag.wifi = True
+			print('Get check status request ->',end = '')
+			
+			httpResponse.WriteResponseOk(headers = None,
+				contentType	 = "text/html",
+				contentCharset = "UTF-8",
+				content = 'OK')
+			if content == 'OK':
+				print('completed')
+				self.server.setup_success()
+		else :
+			core.flag.wifi = False
+			print('.' , end = '')
+			httpResponse.WriteResponseOk(headers = None,
+				contentType	 = "text/html",
+				contentCharset = "UTF-8",
+				content = 'Failed')
 		
 		"""
 		if self.wifi_status == 1:
@@ -78,46 +114,16 @@ class BootMode :
 		self.wifi_status = 0
 		httpResponse.WriteResponseOk(headers = None,contentType= "text/html",	contentCharset = "UTF-8",content = 'OK')
 		self.wlan_sta.connect(request_json['ssid'], request_json['password'])
-		print('client->saveconfig: Trying to connect to ' + str(request_json) , end = '')
-		for x in range(130):
+		
+		#
+		while not self.wlan_sta.isconnected() :
 			core.time.sleep_ms(100)
-			if self.wlan_sta.isconnected():
-				self.wifi_status = 1
-				print('Connected to ' , request_json['ssid'])
-				config = {}
-				try :
-					config = core.json.loads(open('config.json').read())
-				except :
-					pass
-				if not config.get('known_networks'):
-					config['known_networks'] = [{'ssid': request_json['ssid'], 'password': request_json['password']}]
-				else:
-					exist_ssid = None
-					for n in config['known_networks']:
-						if n['ssid'] == request_json['ssid']:
-							exist_ssid = n
-							break
-					if exist_ssid:
-						exist_ssid['password'] = request_json['password'] # update WIFI password
-						print('Update wifi password')
-					else:
-						# add new WIFI network
-						config['known_networks'].append({'ssid': request_json['ssid'], 'password': request_json['password']})
-						print('Add new network')
-				print('Devicename')
-				if len(request_json['deviceName']):
-					config['device_name'] = request_json['deviceName']
-				if len(request_json['token']):
-					config['token'] = request_json['token']
-				
-				f = open('config.json', 'w')
-				f.write(core.json.dumps(config))
-				f.close()
-				core.flag.wifi = True
-				break
-			else :
-				core.flag.wifi = False
-				print('.' , end = '')
+			print('.',end = '')
+		core.machine.reset()
+		#
+		
+		print('client->saveconfig: Trying to connect to ' + str(request_json) , end = '')
+		# we cant wait until the network to be connect here !
 	def is_ascii(self, s):
 		return all(ord(c) < 128 for c in s)
 	
@@ -128,10 +134,10 @@ class BootMode :
 		networks = []
 		raw = self.wlan_sta.scan()
 		for nw in raw:
-			networks.append({'ssid': nw[0].decode('ascii'), 'rssi': nw[3]})
-		
-		content = core.json.dumps(networks)
-		print(len(networks) , 'networks detected')
+			networks.append({'ssid': nw[0], 'rssi': nw[3]})
+		content = core.json.dumps(networks).encode('utf-8')
+		print(content)
+		print(len(networks) , len(content) , type(content) , 'networks detected')
 		httpResponse.WriteResponseOk(headers = None,contentType= "application/json",contentCharset = "UTF-8",content = content)
 		
 	async def Start(self):
@@ -146,7 +152,6 @@ class BootMode :
 		if core.gc.mem_free() < 20000 :
 			core.machine.reset()
 		
-		server = None 
 		id = core.binascii.hexlify(core.machine.unique_id()).decode('ascii')
 		uuid = [id[i:i+2] for i in range(0, len(id), 2)]
 
@@ -194,13 +199,14 @@ class BootMode :
 		]
 		
 		from Blocky.MicroWebSrv import MicroWebSrv
-		server = MicroWebSrv(routeHandlers = routeHandlers)
+		self.server = MicroWebSrv(routeHandlers = routeHandlers)
 		print('bootmode-> started')
 		#loop = asyncio.get_event_loop()
 		#loop.create_task(server.Start())
 		
-		await server.Start()
+		await self.server.Start()
 		print('bootmode-> completed')
+			
 		from machine import reset
 		reset()
 	
--- src/MicroWebSrv.py
+++ src/MicroWebSrv.py
@@ -10,23 +10,23 @@ class MicroWebSrv :
 		"index.htm",
 	]
 	_mimeTypes = {
-		".txt"   : "text/plain",
-		".htm"   : "text/html",
-		".html"  : "text/html",
-		".css"   : "text/css",
-		".csv"   : "text/csv",
+		".txt"	 : "text/plain",
+		".htm"	 : "text/html",
+		".html"	: "text/html",
+		".css"	 : "text/css",
+		".csv"	 : "text/csv",
 		".js"	: "application/javascript",
-		".xml"   : "application/xml",
+		".xml"	 : "application/xml",
 		".xhtml" : "application/xhtml+xml",
-		".json"  : "application/json",
-		".zip"   : "application/zip",
-		".pdf"   : "application/pdf",
-		".jpg"   : "image/jpeg",
-		".jpeg"  : "image/jpeg",
-		".png"   : "image/png",
-		".gif"   : "image/gif",
-		".svg"   : "image/svg+xml",
-		".ico"   : "image/x-icon"
+		".json"	: "application/json",
+		".zip"	 : "application/zip",
+		".pdf"	 : "application/pdf",
+		".jpg"	 : "image/jpeg",
+		".jpeg"	: "image/jpeg",
+		".png"	 : "image/png",
+		".gif"	 : "image/gif",
+		".svg"	 : "image/svg+xml",
+		".ico"	 : "image/x-icon"
 	}
 	_html_escape_chars = {
 		"&" : "&amp;",
@@ -37,7 +37,7 @@ class MicroWebSrv :
 	}
 	_pyhtmlPagesExt = '.pyhtml'
 	
-	# ===( Utils  )===============================================================
+	# ===( Utils	)===============================================================
 	
 	def HTMLEscape(s) :
 		return ''.join(MicroWebSrv._html_escape_chars.get(c, c) for c in s)
@@ -87,19 +87,19 @@ class MicroWebSrv :
 	# ===( Constructor )==========================================================
 	
 	def __init__( self,
-				  routeHandlers = None,
-				  port		  = 80,
-				  bindIP		= '0.0.0.0',
-				  webPath	   = "/flash/www" ) :
+					routeHandlers = None,
+					port			= 80,
+					bindIP		= '0.0.0.0',
+					webPath		 = "/flash/www" ) :
 		self._routeHandlers = routeHandlers
-		self._srvAddr	   = (bindIP, port)
-		self._webPath	   = webPath
-		self._notFoundUrl   = None
-		self._started	   = False
+		self._srvAddr		 = (bindIP, port)
+		self._webPath		 = webPath
+		self._notFoundUrl	 = None
+		self._started		 = False
 		self.MaxWebSocketRecvLen	 = 1024
-		self.WebSocketThreaded	   = True
+		self.WebSocketThreaded		 = True
 		self.AcceptWebSocketCallback = None
-		
+		self.success = False
 		self._socket = None 
 		self.bootdone = False
 	# ===( Server Process )=======================================================
@@ -110,13 +110,13 @@ class MicroWebSrv :
 			wlan_ap = core.network.WLAN(core.network.AP_IF)
 			wlan_ap.active(True)
 			print('START AP')
-			while core.flag.wifi == False :
-				await core.asyncio.sleep_ms(200)
-				if wlan_ap.isconnected():
+			while True :
+				await core.asyncio.sleep_ms(50)
+				
+				if wlan_ap.isconnected() and self.success == False:
 					try :
 						try :
 							client, cliAddr = self._socket.accept()
-							
 						except :
 							continue
 						print(client , cliAddr)
@@ -126,21 +126,23 @@ class MicroWebSrv :
 					except Exception as err :
 						print('client->',err)
 						core.sys.print_exception(err)
-				
+				if self.success == True :
+					break
 			self._started = False
 			print('CLOSE AP')
 		except Exception as err:
 			print( ' _socket -> ' , err)
 	
 	# ===( Functions )============================================================
-	
+	def setup_success(self):
+		self.success = True
 	async def Start(self, threaded=True) :
 		if not self._started :
 			reset_timer = core.machine.Timer(1)
 			reset_timer.init(mode=core.machine.Timer.ONE_SHOT,period = 1800000,callback =lambda t:core.machine.reset())
 			self._socket = core.socket.socket( core.socket.AF_INET,
-										  core.socket.SOCK_STREAM,
-										  core.socket.IPPROTO_TCP )
+											core.socket.SOCK_STREAM,
+											core.socket.IPPROTO_TCP )
 			self._socket.setsockopt( core.socket.SOL_SOCKET,
 									 core.socket.SO_REUSEADDR,
 									 1 )
@@ -173,9 +175,9 @@ class MicroWebSrv :
 			method = method.upper()
 			for route in self._routeHandlers :
 				if len(route) == 3 and			\
-				   route[0].upper() == resUrl and \
-				   route[1].upper() == method :
-				   return route[2]
+					 route[0].upper() == resUrl and \
+					 route[1].upper() == method :
+					 return route[2]
 		return None
 
 	def _physPathFromURLPath(self, urlPath) :
@@ -190,23 +192,23 @@ class MicroWebSrv :
 				return physPath
 		return None
 	
-	# ===( Class Client  )========================================================
+	# ===( Class Client	)========================================================
 	
 	class _client :
 		
 		def __init__(self, microWebSrv, socket, addr) :
 			socket.settimeout(2)
-			self._microWebSrv   = microWebSrv
+			self._microWebSrv	 = microWebSrv
 			self._socket		= socket
-			self._addr		  = addr
+			self._addr			= addr
 			self._method		= None
-			self._path		  = None
-			self._httpVer	   = None
-			self._resPath	   = "/"
-			self._queryString   = ""
-			self._queryParams   = { }
-			self._headers	   = { }
-			self._contentType   = None
+			self._path			= None
+			self._httpVer		 = None
+			self._resPath		 = "/"
+			self._queryString	 = ""
+			self._queryParams	 = { }
+			self._headers		 = { }
+			self._contentType	 = None
 			self._contentLength = 0
 			#await self._processRequest()
 		
@@ -230,9 +232,9 @@ class MicroWebSrv :
 							 and self._microWebSrv.AcceptWebSocketCallback :
 								MicroWebSocket( socket		 = self._socket,
 												httpClient	 = self,
-												httpResponse   = response,
+												httpResponse	 = response,
 												maxRecvLen	 = self._microWebSrv.MaxWebSocketRecvLen,
-												threaded	   = self._microWebSrv.WebSocketThreaded,
+												threaded		 = self._microWebSrv.WebSocketThreaded,
 												acceptCallback = self._microWebSrv.AcceptWebSocketCallback )
 								return
 						else :
@@ -245,17 +247,18 @@ class MicroWebSrv :
 			try :
 				print('Socket Close')
 				self._socket.close()
-			except :
+			except Exception as err:
+				import sys; sys.print_exception(err)
 				pass
 		
 		def _parseFirstLine(self, response) :
 			try :
 				elements = self._socket.readline().decode().strip().split()
 				if len(elements) == 3 :
-					self._method  = elements[0].upper()
+					self._method	= elements[0].upper()
 					self._path	= elements[1]
 					self._httpVer = elements[2].upper()
-					elements	  = self._path.split('?', 1)
+					elements		= self._path.split('?', 1)
 					if len(elements) > 0 :
 						self._resPath = MicroWebSrv._unquote_plus(elements[0])
 						if len(elements) > 1 :
@@ -279,7 +282,7 @@ class MicroWebSrv :
 					self._headers[elements[0].strip()] = elements[1].strip()
 				elif len(elements) == 1 and len(elements[0]) == 0 :
 					if self._method == 'POST' :
-						self._contentType   = self._headers.get("Content-Type", None)
+						self._contentType	 = self._headers.get("Content-Type", None)
 						self._contentLength = int(self._headers.get("Content-Length", 0))
 					return True
 				else :
@@ -304,7 +307,7 @@ class MicroWebSrv :
 			return b if b else b''
 		
 		def ReadRequestPostedFormData(self) :
-			res  = { }
+			res	= { }
 			data = self.ReadRequestContent()
 			if len(data) > 0 :
 				elements = data.decode().split('&')
@@ -316,7 +319,7 @@ class MicroWebSrv :
 			return res
 		
 	
-	# ===( Class Response  )======================================================
+	# ===( Class Response	)======================================================
 	
 	class _response :
 		
@@ -326,11 +329,13 @@ class MicroWebSrv :
 		def _write(self, data) :
 			try :
 				
-				if isinstance(data , str):
+				if isinstance(data , str) or isinstance(data , bytes):
+					print('socket_write' , data , len(data))
 					return self._client._socket.write(data)
 				elif isinstance(data , list):
 					for x in data :
 						self._client._socket.write(x)
+					
 				
 			except Exception as err:
 				print('socket->_write->' , err)
@@ -346,7 +351,7 @@ class MicroWebSrv :
 		def _writeContentTypeHeader(self, contentType, charset=None) :
 			if contentType :
 				ct = contentType \
-				   + (("; charset=%s" % charset) if charset else "")
+					 + (("; charset=%s" % charset) if charset else "")
 			else :
 				ct = "application/octet-stream"
 			self._writeHeader("Content-Type", ct)
@@ -366,40 +371,54 @@ class MicroWebSrv :
 					self._writeHeader(header, headers[header])
 		
 		def WriteResponse(self, code, headers, contentType, contentCharset, content) :
-			contentCharset = None
 			try :
-				if isinstance(content , str):
+				
+				if isinstance(content , str) or isinstance(content , bytes):
 					contentLength = len(content) if content else 0
+					print('LEN ' , contentLength , contentCharset , contentType )
 				elif isinstance(content , list):
 					contentLength = 0
 					for x in content :
 						contentLength+= len(x)
+				else :
+					contentLength = 0
 				#self._writeBeforeContent(code, headers, contentType, contentCharset, contentLength)
 				
 				self._writeFirstLine(code)
 				if isinstance(headers, dict) :
 					for header in headers :
 						self._writeHeader(header, headers[header])
+				
 				if contentLength > 0 :
 					#self._writeContentTypeHeader(contentType, contentCharset)
+					ct = None
+					
 					if contentType :
+						
 						ct = contentType \
-						   + (("; charset=%s" % contentCharset) if contentCharset else "")
+							 + (("; charset=%s" % contentCharset) if contentCharset else "")
+						
 					else :
 						ct = "application/octet-stream"
 					self._writeHeader("Content-Type", ct)
 					self._writeHeader("Content-Length", contentLength)
+				
 				self._writeHeader("Server", "MicroWebSrv by JC`zic")
 				self._writeHeader("Connection", "close")
 				self._write("\r\n")	#self._writeEndHeader()
 				
 				if contentLength > 0 :
 					self._write(content)
+					print('_write',len(content))
 				
 				return True
 			except MemoryError as err:
 				print('mwsr->wR',err)
 				return False
+			except Exception as err :
+				import sys
+				sys.print_exception(err)
+				
 		
 
 		def WriteResponseOk(self, headers=None, contentType=None, contentCharset=None, content=None) :
@@ -410,21 +429,21 @@ class MicroWebSrv :
 		def WriteResponseError(self, code) :
 			responseCode = self._responseCodes.get(code, ('Unknown reason', ''))
 			return self.WriteResponse( code,
-									   None,
-									   "text/html",
-									   "UTF-8",
-									   self._errCtnTmpl % {
+										 None,
+										 "text/html",
+										 "UTF-8",
+										 self._errCtnTmpl % {
 											'code'	: code,
-											'reason'  : responseCode[0],
+											'reason'	: responseCode[0],
 											'message' : responseCode[1]
-									   } )
+										 } )
 		
 		def WriteResponseJSONError(self, code, obj=None) :
 			return self.WriteResponse( code,
-									   None,
-									   "application/json",
-									   "UTF-8",
-									   dumps(obj if obj else { }) )
+										 None,
+										 "application/json",
+										 "UTF-8",
+										 dumps(obj if obj else { }) )
 		
 		def WriteResponseBadRequest(self) :
 			return self.WriteResponseError(400)
